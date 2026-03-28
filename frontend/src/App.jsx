0cd2ef0a194a0d5bb7fea6d5b085c67e3b70a2f7
Dhanush M <310623104036@eec.srmrmp.edu.in>
2026-03-28 22:50:47 +0000

    feat: Wrap application in ThemeProvider for improved theming support

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
 import { AppProvider } from './context/AppContext'
+import { ThemeProvider } from './context/ThemeContext'
 import { ProtectedRoute } from './components/ProtectedRoute'
 import { Layout } from './components/Layout'
 import { LoginPage } from './pages/LoginPage'
@@ -13,40 +14,42 @@ import { TasksPage } from './pages/TasksPage'
 function App() {
   return (
     <BrowserRouter>
-      <AuthProvider>
-        <AppProvider>
-          <Routes>
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignupPage />} />
-            
-            <Route path="/" element={
-              <ProtectedRoute>
-                <Layout>
-                  <DashboardPage />
-                </Layout>
-              </ProtectedRoute>
-            } />
-            
-            <Route path="/employees" element={
-              <ProtectedRoute>
-                <Layout>
-                  <EmployeesPage />
-                </Layout>
-              </ProtectedRoute>
-            } />
-            
-            <Route path="/tasks" element={
-              <ProtectedRoute>
-                <Layout>
-                  <TasksPage />
-                </Layout>
-              </ProtectedRoute>
-            } />
+      <ThemeProvider>
+        <AuthProvider>
+          <AppProvider>
+            <Routes>
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/signup" element={<SignupPage />} />
+              
+              <Route path="/" element={
+                <ProtectedRoute>
+                  <Layout>
+                    <DashboardPage />
+                  </Layout>
+                </ProtectedRoute>
+              } />
+              
+              <Route path="/employees" element={
+                <ProtectedRoute>
+                  <Layout>
+                    <EmployeesPage />
+                  </Layout>
+                </ProtectedRoute>
+              } />
+              
+              <Route path="/tasks" element={
+                <ProtectedRoute>
+                  <Layout>
+                    <TasksPage />
+                  </Layout>
+                </ProtectedRoute>
+              } />
 
-            <Route path="*" element={<Navigate to="/" />} />
-          </Routes>
-        </AppProvider>
-      </AuthProvider>
+              <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+          </AppProvider>
+        </AuthProvider>
+      </ThemeProvider>
     </BrowserRouter>
   )
 }
